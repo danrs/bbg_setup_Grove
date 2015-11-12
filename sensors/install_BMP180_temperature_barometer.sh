c1c4138f3a6d0a1ef98b0db2429711947ed74a9b
Daniel Smith <danrs@users.noreply.github.com>
2015-11-12 19:04:21 +0000

    added bmp180 install file

---
+++ sensors/install_BMP180_temperature_barometer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone https://github.com/adafruit/Adafruit_Python_BMP.git
+cd Adafruit_Python_BMP
+sudo python setup.py install
+
+# Information on usage can be found here:
+# https://learn.adafruit.com/using-the-bmp085-with-raspberry-pi/using-the-adafruit-bmp-python-library
+#
+# ALternately, look at the readme and examples in the downloaded repository
