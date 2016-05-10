f3043737e7e3acd99b2be9f7eb19f1bf74d42da4
Daniel Smith <danrs@users.noreply.github.com>
2016-05-10 00:41:50 +0000

    added reboot and y/n interactivity to setup script

--- setup.sh
+++ setup.sh
@@ -2,10 +2,9 @@
 
 # This script sets up the BB green/black for use with Grove modules
 
-# Continue function for y/n interactivity
-continueYN () {
-    # call with a prompt string or use a default
-    read -r -p "${1:-Continue anyway? [y/N]} " response
+# Functions for y/n interactivity
+response_yN () {
+    read -r -p "${1:-Continue? [y/N]} " response
     case $response in
         [yY][eE][sS]|[yY]) 
             true
@@ -16,21 +15,34 @@ continueYN () {
     esac
 }
 
+response_Yn () {
+    read -r -p "${1:-Continue? [Y/n]} " response
+    case $response in
+        [nN][oO]|[nN]) 
+            false
+            ;;
+        *)
+            true
+            ;;
+    esac
+}
+
+
 # Script starts here
 echo "Testing internet connection..."
 case "$(curl -s --max-time 5 -I http://google.om | head -1 | sed 's/^[^ ]*  *\([0-9]\).*/\1/')" in
     [23]) echo "Internet connection (HTTP) is up";;
     5) echo "Web proxy seems to be blocking internet access."
-        continueYN || exit;;
+        response_yN "Continue anyway? [y/N]" || exit;;
     *) echo "Network appears to be down or lagging."
-        continueYN || exit;;
+        response_yN "Continue anyway? [y/N]" || exit;;
 esac
 
 echo "Setting up BeagleBone..."
 # set date and time
 sudo ntpdate pool.ntp.org
 
-# Update kernel
+# Update kernel (may require a reboot, untested)
 # Uncomment the line below to update the kernel (may improve WiFi performance)
 #sudo /opt/scripts/tools/update_kernel.sh
 
@@ -42,9 +54,13 @@ sudo pip install Adafruit-BBIO
 # Install ssh server to allow ssh in over WiFi
 sudo apt-get install openssh-server
 
-#TODO  add instructions to readme to set static ip on router
+# Enable UART
+sudo echo "cape_enable=capemgr.enable_partno=BB-UART2" >> /boot/uEnv.txt # UART 2 only
+#sudo echo "cape_enable=capemgr.enable_partno=BB-UART1,BB-UART2" >> /boot/uEnv.txt # UART 1 and 2
 
-#TODO enable UART (see p49 of log book)
+echo "Installations complete!"
 
-#TODO add other scripts to install existing repos for individual sensors
+# Reboot to apply changes
+echo "Reboot required for changes to take effect."
+response_Yn "Reboot now? [Y/n]" && sudo reboot
 
