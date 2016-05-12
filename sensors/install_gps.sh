220faa7dd8d46df87011e21a6afad7645a7a347a
Dan Smith <drsmith900@gmail.com>
2016-05-12 14:08:27 +1000

    added info on starting gpsd

--- sensors/install_gps.sh
+++ sensors/install_gps.sh
@@ -5,8 +5,14 @@
 # See the page below for usage information (grove gps is very similar to this one):
 # https://learn.adafruit.com/adafruit-ultimate-gps-on-the-raspberry-pi/using-your-gps
 #
+# Before using gps in python, start the client:
+# $ sudo killall gpsd
+# $ sudo gpsd /dev/ttyO2 -F /ver/run/gpsd.sock # ttyO2 is the device for UART2
+#
 # You can also test your sensor on the command line with:
 # $ cgps -s
+# or:
+# $ gpsmon
 #
 
 sudo apt-get update
